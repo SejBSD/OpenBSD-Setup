2c49094fdfb87b1d80c777d6bd9ae4eb266e3b01
Krzysztof Dobrzyński <k.dobrzynski94@gmail.com>
2023-01-24 12:06:12 +0000

    split to setup services;

---
+++ Setup_Services.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+pkg_add -u
+
+echo ""
+echo "################################################################"
+echo "##                                                            ##"
+echo "##                   Setting up Services...                   ##"
+echo "##                                                            ##"
+echo "################################################################"
+echo ""
+
+echo ""
+echo "################################################################"
+echo "##                                                            ##"
+echo "##                  Setting up phpMyAdmin...                  ##"
+echo "##                                                            ##"
+echo "################################################################"
+echo ""
+
+echo "Configure phpMyAdmin? (yes/empty)"
+read _phpMyAdmin;
+
+if [ "$_phpMyAdmin" != "" ]
+then
+  pkg_add phpmyadmin
+fi
+
+echo ""
+echo "################################################################"
+echo "##                                                            ##"
+echo "##                     Setting up NGINX...                    ##"
+echo "##                                                            ##"
+echo "################################################################"
+echo ""
+
+echo "Configure NGINX? (yes/empty)"
+read _nginx;
+
+if [ "$_nginx" != "" ]
+then
+  pkg_add nginx
+fi
+
+echo ""
+echo "################################################################"
+echo "##                                                            ##"
+echo "##              Setting up Apache HTTP Server...              ##"
+echo "##                                                            ##"
+echo "################################################################"
+echo ""
+
+echo "Configure Apache HTTP Server? (yes/empty)"
+read _apacheHttp;
+
+if [ "$_apacheHttp" != "" ]
+then
+  pkg_add apache-httpd
+fi
